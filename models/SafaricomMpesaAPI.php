f254f36ef4d7a12dcbfe519a7db6d0a1e18ebbc6
David <davidkariuki259@gmail.com>
2026-01-07 14:12:00 +0300

    skeleton for api model

---
+++ models/SafaricomMpesaAPI.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * ContactForm is the model behind the contact form.
+ */
+class SafaricomMpesaAPI extends Model{
+
+    //declare url constants
+
+    //define basic curl function
+
+    //define required API methods
+}
