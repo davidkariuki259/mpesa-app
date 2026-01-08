1aed9fecd32b4c9b515626a207098efd4ad77ccc
David <davidkariuki259@gmail.com>
2026-01-08 14:42:22 +0300

    default db configs

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=bernard_mpesa',
-    'username' => 'newuser',
-    'password' => 'password',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
