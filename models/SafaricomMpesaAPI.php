f30c2cd4d5271f8736369a469920bc2c05e0d6a4
David <davidkariuki259@gmail.com>
2026-01-08 15:40:37 +0300

    initial version for mpesa api file

--- models/SafaricomMpesaAPI.php
+++ models/SafaricomMpesaAPI.php
@@ -10,6 +10,18 @@ use yii\base\Model;
  */
 class SafaricomMpesaAPI extends Model{
 
+    public $consumer_key;
+    public $consumer_secret;
+    public $token;
+
+    public function __construct()
+    {
+        $api_credentials = (new ApiCredential())->findOne();
+        $this->consumer_key = $api_credentials->consumer_key;
+        $this->consumer_secret = $api_credentials->consumer_secret;
+        $this->token = $api_credentials->token;
+    }
+    
     //declare url constants
 
     //define basic curl function
